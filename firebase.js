811fbfbc59be08e7b6445239f18bfab22a3795a3
Nastya <nshpv@icloud.comgit config --global user.email n.shpv@icloud.com'git config --global user.email n.shpv@icloud.com>
2024-04-19 22:45:10 +0300

    first commit

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDZ7vmk6lTKcZvl4XMDe05lqvIyUQGUrUM",
+    authDomain: "react-app-f8b94.firebaseapp.com",
+    projectId: "react-app-f8b94",
+    storageBucket: "react-app-f8b94.appspot.com",
+    messagingSenderId: "828606670398",
+    appId: "1:828606670398:web:ffe0b47c6d3f059ce2936f",
+    measurementId: "G-ZD6DG1JRGF",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
